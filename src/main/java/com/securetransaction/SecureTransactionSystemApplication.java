f19bca6bac419da4f7dac3b5eb69b71e7d34c20a
Kush Kansal <mrkushkansal@gmail.com>
2026-03-03 01:12:13 +0530

    initial commit

---
+++ src/main/java/com/securetransaction/SecureTransactionSystemApplication.java
@@ -0,0 +1,13 @@
+package com.securetransaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureTransactionSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureTransactionSystemApplication.class, args);
+	}
+
+}
